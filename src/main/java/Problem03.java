669f9fb89b5355a41b59359baf95537fbe75bd47
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2022-04-26 01:45:17 -0400

    Final commit

--- src/main/java/Problem03.java
+++ src/main/java/Problem03.java
@@ -3,27 +3,32 @@ public class Problem03 {
     
     public static int calcPascal(int n) {
         int sum = 0;
-        for(int row = 1; row <= n; row++) { // n
+        for(int row = 1; row <= n; row++) {
             long C = 1;
-            for(int el = 1; el <= row; el++) { // n
+            for(int el = 1; el <= row; el++) {
                 
                 if (C % 7 == 0) {
                     sum++;
+                    //System.out.print(C+" ");
+                    System.out.print(".");
+                } else {
+                    //System.out.print("(" + C +") ");
+                    System.out.print("1");
+                    
                 }
-                //System.out.print(C + " ");
+                
                 C = C * (row - el) / el;
             }
-            //System.out.println();
+            System.out.println();
         }
         
         return sum;
     }
     public static void main(String[] args) {
-        int n = 100;
+        int n = 200;
+        int count = n*(n+1) / 2;
+        System.out.println(count);
         System.out.println(calcPascal(n));
     }
-    
-    
-    // Time complexity O(n^2)
-    // Space complexity O(1)
+   
 }
